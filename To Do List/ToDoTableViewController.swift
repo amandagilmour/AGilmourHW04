09f825677bfc55511fe1b5b86c24cabfca0f18bc
Amanda Gilmour <amandagilmour@amandas-macbook-air.local>
2017-02-12 20:25:06 -0500

    5.3 changes

--- To Do List/ToDoTableViewController.swift
+++ To Do List/ToDoTableViewController.swift
@@ -95,5 +95,13 @@ class ToDoTableViewController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    @IBAction func unwindToTableViewController(sender: UIStoryboardSegue) {
+        if let sourceViewController = sender.source as? DetailViewController, let ToDoItem = sourceViewController.toDoItem {
+            toDoArray.append(ToDoItem)
+            let newIndexPath = IndexPath(row: toDoArray.count - 1, section: 0)
+            tableView.insertRows(at: [newIndexPath], with: .bottom)
+        }
+    }
 
 }
