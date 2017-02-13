90a73b01cc8e0b0eaf70ede6dc9402ef7ee229ac
Amanda Gilmour <amandagilmour@amandas-macbook-air.local>
2017-02-13 15:42:21 -0500

    Final Ch. 5

--- To Do List/ToDoTableViewController.swift
+++ To Do List/ToDoTableViewController.swift
@@ -10,7 +10,13 @@ import UIKit
 
 class ToDoTableViewController: UITableViewController {
     
-    var toDoArray = ["Learn Swift", "Build Apps", "Change the World!"]
+    @IBOutlet weak var addBarButton: UIBarButtonItem!
+    
+    var toDoArray = [String]()
+    
+    var toDoNotes = [String]()
+    
+    var defaultsData = UserDefaults.standard
     
 
     override func viewDidLoad() {
@@ -20,7 +26,19 @@ class ToDoTableViewController: UITableViewController {
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        self.navigationItem.leftBarButtonItem = self.editButtonItem
+        
+        toDoArray = defaultsData.stringArray(forKey: "toDoArray") ?? [String]()
+        toDoNotes = defaultsData.stringArray(forKey: "toDoNotes") ?? [String]()
+        
+        // If one array is count size zero, set the other one to an empty array as well
+        if toDoArray.count == 0 {
+            toDoNotes = [String]()
+        }
+        if toDoNotes.count == 0 {
+            toDoArray = [String]()
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -46,62 +64,126 @@ class ToDoTableViewController: UITableViewController {
 
         // Configure the cell...
         cell.textLabel?.text = toDoArray[indexPath.row]
+        cell.detailTextLabel?.text = toDoNotes[indexPath.row]
 
         return cell
     }
     
 
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+ 
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            toDoArray.remove(at: indexPath.row) // remove from array
+            toDoNotes.remove(at: indexPath.row) // remove selected note from note array
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
+        defaultsData.set(toDoArray, forKey: "toDoArray")
+        defaultsData.set(toDoNotes, forKey: "toDoNotes")
     }
-    */
+ 
 
-    /*
+    
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+        
+        // First make a copy of the values in the cell we are moving
+        let itemToMove = toDoArray[fromIndexPath.row]
+        let noteToMove = toDoNotes[fromIndexPath.row]
+        
+        // Delete them from the original location (pre-move)
+        toDoArray.remove(at: fromIndexPath.row)
+        toDoNotes.remove(at: fromIndexPath.row)
+        
+        // Insert them to the "to", post-move location
+        toDoArray.insert(itemToMove, at: to.row)
+        toDoNotes.insert(noteToMove, at: to.row)
+        
+        // Save to UserDefaults
+        defaultsData.set(toDoArray, forKey: "toDoArray")
+        defaultsData.set(toDoNotes, forKey: "toDoNotes")
     }
-    */
+ 
 
-    /*
+    
     // Override to support conditional rearranging of the table view.
     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the item to be re-orderable.
         return true
     }
-    */
-
-    /*
+ 
+    
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        
+        super.setEditing(editing, animated: animated)
+        
+        if (editing) {
+            addBarButton.isEnabled = false
+        } else {
+            addBarButton.isEnabled = true
+        }
+    }
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        
+        if segue.identifier == "ShowDetail" {
+            let indexPath = tableView.indexPathForSelectedRow!
+            let destinationViewController = segue.destination as! DetailViewController
+            let selectToDo = toDoArray[indexPath.row]
+            let selectToDoNote = toDoNotes[indexPath.row]
+            destinationViewController.toDoItem = selectToDo
+            destinationViewController.toDoNote = selectToDoNote
+            
+        }
     }
-    */
+ 
     
     @IBAction func unwindToTableViewController(sender: UIStoryboardSegue) {
-        if let sourceViewController = sender.source as? DetailViewController, let ToDoItem = sourceViewController.toDoItem {
-            toDoArray.append(ToDoItem)
-            let newIndexPath = IndexPath(row: toDoArray.count - 1, section: 0)
-            tableView.insertRows(at: [newIndexPath], with: .bottom)
-        }
+        if let sourceViewController = sender.source as? DetailViewController, let toDoItem = sourceViewController.toDoItem {
+            
+            if let selectIndexPath = tableView.indexPathForSelectedRow {
+                toDoArray[selectIndexPath.row] = toDoItem
+                if let toDoNote = sourceViewController.toDoNote {
+                    toDoNotes[selectIndexPath.row] = toDoNote
+                } else {
+                    toDoNotes[selectIndexPath.row] = ""
+                }
+                tableView.reloadRows(at: [selectIndexPath], with: .none)
+            } else {
+                // Add a new item to a array & the tableView
+                let newIndexPath = IndexPath(row: toDoArray.count, section: 0)
+                
+                toDoArray.append(toDoItem)
+                if let toDoNote = sourceViewController.toDoNote {
+                    toDoNotes.append(toDoNote)
+                } else {
+                    toDoNotes.append("")
+                }
+              
+                tableView.insertRows(at: [newIndexPath], with: .bottom)
+
+            }
+            
+            defaultsData.set(toDoArray, forKey: "toDoArray")
+            defaultsData.set(toDoNotes, forKey: "toDoNotes")
+                
+                    }
     }
 
 }
